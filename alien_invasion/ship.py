64f30da3421e3ce56e9870af8678cfd582cf45ae
sunkeling <454541398@qq.com>
2022-11-30 23:55:03 +0800

    限制飞船活动范围

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -4,6 +4,9 @@ class Ship:
     def __init__(self, ai_game=None):
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.seetings
+
+        self.x = float(self.rect.x)
 
         # 加载飞船图像并获取外界矩形
         self.image = pygame.image.load('images/ship.bmp')
@@ -17,10 +20,12 @@ class Ship:
         self.moving_left = False
 
     def update(self):
-        if self.moving_right:
-            self.rect += 1
-        if self.moving_left:
-            self.rect += 1
+        """根据移动标志调整飞船的位置"""
+        if self.moving_left and self.rect.left > 0:
+            self.rect -= self.settings.ship_speed
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect += self.settings.ship_speed
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
